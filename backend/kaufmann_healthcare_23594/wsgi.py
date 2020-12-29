08c5c6b9739f903012b484801021062f98b89a66
Crowdbotics <team@crowdbotics.com>
2020-12-29 14:32:10 +0000

    Update models

--- backend/kaufmann_healthcare_23594/wsgi.py
+++ backend/kaufmann_healthcare_23594/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaufmann_healthcare_23594.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kaufmann_healthcare_23594.settings")
 
 application = get_wsgi_application()
